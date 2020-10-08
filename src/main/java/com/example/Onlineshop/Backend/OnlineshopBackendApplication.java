f210d4f458be9a289cb607fe5eb86bff18cfafa8
imransuet <imransuet@gmail.com>
2020-10-08 22:36:53 +0200

    first commit

---
+++ src/main/java/com/example/Onlineshop/Backend/OnlineshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Onlineshop.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Onlineshop/Backend/OnlineshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Onlineshop.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
